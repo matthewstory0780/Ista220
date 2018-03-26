f80032145612de56ba271c427708f17ef6a64603
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-26 11:57:30 -0400

    Add files via upload

---
+++ exercises/mathforumals/arrayexcersise/arrayexcersise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] i = new int[6] { 0, 2, 4, 6, 8, 10 };
+            int size = i.Length;
+            int sum = i.Sum();
+            double average = (i.Sum() / i.Length);
+
+            for (int j = 0; j < 6; j++) Console.WriteLine("Array A = {0}", i[j]);
+            Console.WriteLine($"The sum of the elements in the array is {i.Sum()}");
+            Console.WriteLine($"The average of the elements in the array is { (i.Sum()/ i.Length) }");
+
+
+        }
+        
+        
+        
+        
+
+
+    }
+
+}
