c589ac3017d8c2305ecc1ef477006eb1b0de2fa8
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-26 12:01:28 -0400

    Add files via upload

---
+++ exercises/mathforumals/arrayexerciseB/excersise4b/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersise4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {  1, 3, 5, 7, 9 };
+        
+        }
+        public static void reverse(int[] p)
+            {
+            int[] arr = new int[] { 1, 3, 5, 7, 9 };
+            int[] d = new int[p.Length];
+            
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i].ToString());
+            }
+            
+            }
+
+
+    }
+}
---
+++ exercises/mathforumals/arrayexerciseC/exercise4c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 3, 5, 7, 9 };
+            int len = A.Length; //self explanatory 
+            int tmp = A[len - 1]; //save last element value
+            for (int i = len - 1; i > 0; i--) //starting from the end to begining
+            {
+                A[i] = A[i - 1]; //assign value of the previous element
+            }
+            A[0] = tmp; //now "rotate" last to first.
+            Console.WriteLine(A[0] = tmp);
+
+        }
+    }
+}
