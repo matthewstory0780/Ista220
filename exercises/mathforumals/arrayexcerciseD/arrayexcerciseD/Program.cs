69b14edbad44586521ef4ea113ca350bed1aef44
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-26 11:56:16 -0400

    Add files via upload

---
+++ exercises/mathforumals/arrayexcerciseD/arrayexcerciseD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayexcerciseD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 2, 4, 6, 8, 10 };
+            int[] B = { 1, 3, 5, 7, 9 };
+            int[] C = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+            
+
+            RotateArray(1,2, A);
+
+            void RotateArray(int dir, int place, int[] p)
+            {
+                int[] result = new int[p.Length];
+                for (int i = 0; i < p.Length; i++)
+                {
+                    int pos = (i + 2) % p.Length;
+                    Console.WriteLine(result[i] = p[pos]);
+                 }
+
+              
+
+            }
+
+        }
+    }
+}
