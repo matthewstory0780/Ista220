684979496afd222c73be0d9a070f8b70d87fce31
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-25 20:20:03 -0400

    Add files via upload

---
+++ exercises/mathforumals/excersise4b/excersise4b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersise4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {  1, 3, 5, 7, 9 };
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i].ToString());
+            }
+
+        }
+
+
+    }
+}
