a810f6c995f3b3c1174bfb12b60dadfe86e3f1d7
matthewstory0780 <36522359+matthewstory0780@users.noreply.github.com>
2018-03-27 10:11:10 -0400

    Add files via upload

---
+++ labs/Vehicles/Vehicles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine($"Journey by Airplane");
+            Airplane myplane = new Airplane();
+            myplane.StartEngine("Contact");
+            myplane.TakeOff();
+            myplane.Drive();
+            myplane.Land();
+            myplane.StopEngine("whiiirrr");
+
+            Console.WriteLine($"journey by Car");
+            Car mycar = new Car();
+            mycar.StartEngine("BRRRRMMMMM");
+            mycar.Accelerate();
+            mycar.Drive();
+            mycar.brake();
+            mycar.StopEngine("phut phut");
+
+            Console.WriteLine("\nTesting Polymorphism");
+            Vehicle v = mycar;
+            v.Drive();
+            v = myplane;
+            v.Drive();
+ 
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
